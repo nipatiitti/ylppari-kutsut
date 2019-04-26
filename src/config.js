0c7a1e7a9d1b8de52b4048f97d7ca40cea4cf03e
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2019-04-26 21:39:56 +0300

    Added configs

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDEQv-jmJpKRKl9T9XetfUxbOfnxGIceUc',
+    authDomain: 'ylppari.firebaseapp.com',
+    databaseURL: 'https://ylppari.firebaseio.com',
+    projectId: 'ylppari',
+    storageBucket: 'ylppari.appspot.com',
+    messagingSenderId: '97716258147'
+}
